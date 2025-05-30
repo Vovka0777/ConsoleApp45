595d6298d928b1afddfcfefd58f524d0319753e9
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-05-30 12:52:24 +0300

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    class Point<T>
+    {
+        T x;
+        T y;
+        public T X { get; set; }
+        public T Y { get; set; }
+
+        public Point(T x, T y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override string ToString()
+        {
+            return $"{X}, {Y}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> p1 = new Point<int>(3, 4);
+            //string s1 = (string)p1.X;
+            Point<uint> p2 = new Point<uint>(5, 6);
+            Point<double> p3 = new Point<double>(1.5, 2.5);
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p2.ToString());
+            Console.WriteLine(p3.ToString());
+
+            Console.Read();
+        }
+    }
+}
