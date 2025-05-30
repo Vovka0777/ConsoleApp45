0f3e0e89604e2e091d86e4c37f340bf32fc1c9c3
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-05-30 12:51:53 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    class Person<T>
+    {
+        public T Id { get; }
+        public string Name { get; }
+        public Person() { }
+        public Person(T id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person<int> tom = new Person<int>(546, "Tom");
+            //Person<string> bob = new Person<string>("a1", "Bob");
+
+            ////int tomId = (int)tom.Id;
+            ////string bobId = (string)bob.Id;
+
+
+            //int tomId = tom.Id;
+            //string bobId = bob.Id;
+            //Console.WriteLine($"Tom's ID: {tomId}\nBob's ID: {bobId}");
+
+            Company<Person<int>> microsoft = new Company<Person<int>>(tom);
+            Console.WriteLine($"Microsoft's CEO: {microsoft.CEO.Name}\nID: {microsoft.CEO.Id}");
+            Console.ReadKey();
+        }
+    }
+}
