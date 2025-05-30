8632dce577c4a47d68047c12028380fb1e4dbfa1
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-05-30 12:52:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Segment<int> segment1 = new Segment<int>(1, 2, 3, 4);
+            Segment<long> segment2 = new Segment<long>(5, 6, 7, 8);
+            Segment<double> segment3 = new Segment<double>(1.1, 2.2, 3.3, 4.4);
+
+            Console.WriteLine(segment1.ToString());
+            Console.WriteLine($"Length of segment1: {segment1.Length()}");
+            Console.WriteLine(segment2.ToString());
+            Console.WriteLine($"Length of segment2: {segment2.Length()}");
+            Console.WriteLine(segment3.ToString());
+            Console.WriteLine($"Length of segment3: {segment3.Length()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Segmen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Segment <T>
+    {
+        public T x1,x2, y1, y2;
+
+        public T X1 { get => x1; set => x1 = value; }
+        public T X2 { get => x2; set => x2 = value; }
+        public T Y1 { get => y1; set => y1 = value; }
+        public T Y2 { get => y2; set => y2 = value; }
+
+        public Segment(T x1, T y1, T x2, T y2)
+        {
+            this.X1 = x1;
+            this.Y1 = y1;
+            this.X2 = x2;
+            this.Y2 = y2;
+        }
+        public override string ToString()
+        {
+            return $"Segment: ({X1}, {Y1}) to ({X2}, {Y2})";
+        }
+
+        public double Length()
+        {
+
+            return Math.Sqrt((Convert.ToDouble(X2) - Convert.ToDouble(X1)) * (Convert.ToDouble(X2) - Convert.ToDouble(X1)) + (Convert.ToDouble(Y2) - Convert.ToDouble(Y1)) * (Convert.ToDouble(Y2) - Convert.ToDouble(Y1)));
+        }
+    }
+}
